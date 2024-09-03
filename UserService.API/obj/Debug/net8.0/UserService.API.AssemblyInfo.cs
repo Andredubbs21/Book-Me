cf0be5fec1313461c37023698cff2e9c0edc3dc8
Rene Dubon <andredubbs19@unitec.edu>
2024-09-03 17:02:23 -0600

    final update for authentication

--- UserService.API/obj/Debug/net8.0/UserService.API.AssemblyInfo.cs
+++ UserService.API/obj/Debug/net8.0/UserService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ee16f9e95491c6e769fc9344673a54d3e30e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36da544d81141d807aa140b4f900494a35cc8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
